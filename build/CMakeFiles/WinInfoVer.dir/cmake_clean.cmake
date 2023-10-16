569c50fdf8a2bf33e99d8c1a7e7f933065dceffe
MalditoOriginal <maldito.goldpride@gmail.com>
2023-10-17 02:33:04 +0300

    Add build and src

---
+++ build/CMakeFiles/WinInfoVer.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WinInfoVer.dir/gui.c.obj"
+  "CMakeFiles/WinInfoVer.dir/gui.c.obj.d"
+  "CMakeFiles/WinInfoVer.dir/logic.c.obj"
+  "CMakeFiles/WinInfoVer.dir/logic.c.obj.d"
+  "CMakeFiles/WinInfoVer.dir/main.c.obj"
+  "CMakeFiles/WinInfoVer.dir/main.c.obj.d"
+  "WinInfoVer.exe"
+  "WinInfoVer.exe.manifest"
+  "WinInfoVer.pdb"
+  "libWinInfoVer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/WinInfoVer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/logic.c
+++ src/logic.c
@@ -1,37 +1,5 @@
 #include "win_ver.h"
 
-HWND InitializeWindow(HINSTANCE hInstance) {
-    WNDCLASSEX wc = { sizeof(WNDCLASSEX), 
-    CS_CLASSDC, WndProc, 
-    0L, 
-    0L, 
-    hInstance, 
-    NULL, 
-    NULL, 
-    NULL, 
-    NULL, 
-    _T("WinVerApp"), NULL 
-    };
-    if (!RegisterClassEx(&wc)) {
-        return NULL;
-    }
-
-    int winWidth = 400;
-    int winHeight = 200;
-    LPCTSTR winTitle = _T("Windows Version Info");
-
-    HWND hwnd = CreateWindow(wc.lpszClassName, winTitle, WS_OVERLAPPEDWINDOW, 100, 100, winWidth, winHeight, NULL, NULL, wc.hInstance, NULL);
-
-    if (!hwnd) {
-        return NULL;
-    }
-
-    ShowWindow(hwnd, SW_SHOWDEFAULT);
-    UpdateWindow(hwnd);
-
-    return hwnd;
-}
-
 void DisplayVersionInfo(HWND hwnd) {
     OSVERSIONINFOEXA osvi;
     ZeroMemory(&osvi, sizeof(OSVERSIONINFOEXA));
