3b1d0147403c3ceeac17d9e4fe110973eb1cf8a7
MalditoOriginal <maldito.goldpride@gmail.com>
2023-10-19 22:26:14 +0300

    Updated

--- src/logic.cpp
+++ src/logic.cpp
@@ -1,49 +1,27 @@
 #include "global.h"
 
-TCHAR* FormatServicePackInfo(const OSVERSIONINFOEXA& osvi) {
-    TCHAR buffer[256];
-    if (osvi.szCSDVersion[0] != '\0') {
-        _stprintf(buffer, _T("Service Pack: %s"), osvi.szCSDVersion);
-    } else {
-        _stprintf(buffer, _T("Service Pack: None"));
-    }
-    return _tcsdup(buffer);
+// Define a function to check if it's Windows 10 based on the provided conditions.
+bool IsWindows10(const OSVERSIONINFOEX& osvi) {
+    return (osvi.dwMajorVersion == 10 && osvi.dwMinorVersion == 0 &&
+            osvi.wProductType != VER_NT_WORKSTATION) ||
+           (osvi.wProductType == VER_NT_WORKSTATION);
 }
 
 void DisplayVersionInfo(HWND hwnd) {
-    OSVERSIONINFOEXA osvi;
-    ZeroMemory(&osvi, sizeof(OSVERSIONINFOEXA));
-    osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEXA);
+    OSVERSIONINFOEX osvi;
+    ZeroMemory(&osvi, sizeof(OSVERSIONINFOEX));
+    osvi.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEX);
+    TCHAR buffer[256];
 
-    if (GetVersionExA((LPOSVERSIONINFOA)&osvi)) {
-        TCHAR buffer[256];
-        _stprintf(buffer, _T("Windows Version: %d.%d\nBuild Number: %ld"), osvi.dwMajorVersion, osvi.dwMinorVersion, osvi.dwBuildNumber);
-
-        LPCTSTR edition = GetWindowsEdition(osvi.wProductType);
-        _stprintf(buffer, _T("%s\nEdition: %s"), buffer, edition);
-
-        TCHAR* service_pack = FormatServicePackInfo(osvi);
-        _tcscat(buffer, _T("\n")); // Use _tcscat as a safer alternative
-        _tcscat(buffer, service_pack);
-        free(service_pack);
-
+    if (GetVersionEx((LPOSVERSIONINFO)&osvi)) {
+        if (IsWindows10(osvi)) {
+            // Windows 10 detected with the specified conditions
+            _stprintf(buffer, _T("This is Windows 10"));
+        } else {
+            _stprintf(buffer, _T("This is not Windows 10"));
+        }
         MessageBox(hwnd, buffer, _T("Version Info"), MB_ICONINFORMATION | MB_OK);
     } else {
         HandleError(hwnd, _T("Failed to retrieve OS version information."));
     }
-}
-
-
-
-LPCTSTR GetWindowsEdition(WORD productType) {
-    LPCTSTR edition = _T("Unknown");
-
-    if (productType == VER_NT_WORKSTATION) {
-        edition = _T("Client");
-    } else if (productType == VER_NT_SERVER) {
-        edition = _T("Sercer");
-    } else if (productType == VER_NT_DOMAIN_CONTROLLER) {
-        edition = _T("Domain Controller");
-    }
-    return edition;
 }
